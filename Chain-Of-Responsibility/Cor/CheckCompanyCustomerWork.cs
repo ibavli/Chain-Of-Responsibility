7e7b668ff06601c546627924713aa3c967e9525f
ibavli <ibrahim.bavli35@gmail.com>
2019-04-03 00:10:39 +0300

    Müşterinin çalıştığı firma kurumsal bir firma mı kontrolü yapıldı

---
+++ Chain-Of-Responsibility/Cor/CheckCompanyCustomerWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_Of_Responsibility.Entities;
+
+namespace Chain_Of_Responsibility.Cor
+{
+    public class CheckCompanyCustomerWork : CreditRequestBase
+    {
+        public override void ExecuteProcess(Customer customer)
+        {
+            //Bir önceki adımdan geçti mi?
+            if (base.CreditConfirmed)
+            {
+                //Burada müşterinin çalıştığı firmanın iyi bir firma olup olmadığının kontrolü yapılıyor.
+                bool companyIsFine = true;
+
+                base.CreditConfirmed = companyIsFine;
+            }
+        }
+    }
+}

e7b2dc276d6e3ae8ff0c308cc402362b1258b5ae
ibavli <ibrahim.bavli35@gmail.com>
2019-04-03 00:01:28 +0300

    KBB skorunu kontrol eden sınıfımızı yazdık

---
+++ Chain-Of-Responsibility/Cor/CheckKbbScore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_Of_Responsibility.Entities;
+
+namespace Chain_Of_Responsibility.Cor
+{
+    public class CheckKbbScore : CreditRequestBase
+    {
+        public override void ExecuteProcess(Customer customer)
+        {
+            //Bu değeri bir servisten veya veri tabanından çektiğimizi varsayalım.
+            int kbbScore = 1200;
+
+            base.CreditConfirmed = kbbScore > 1000 ? true : false;
+        }
+    }
+}

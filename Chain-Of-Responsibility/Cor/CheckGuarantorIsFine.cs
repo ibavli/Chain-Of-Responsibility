213a6f7838b8ffb163667aaa80742137baee687f
ibavli <ibrahim.bavli35@gmail.com>
2019-04-03 00:06:55 +0300

    Müşterinin kefili iyi mi kontrolü yapıldı

---
+++ Chain-Of-Responsibility/Cor/CheckGuarantorIsFine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_Of_Responsibility.Entities;
+
+namespace Chain_Of_Responsibility.Cor
+{
+    public class CheckGuarantorIsFine : CreditRequestBase
+    {
+        public override void ExecuteProcess(Customer customer)
+        {
+            //Bir önceki adımdan geçti mi?
+            if (base.CreditConfirmed)
+            {
+                //Müşterinin kefili varsa, bu kefilin sağlam olup olmadığına bakılıyor diye düşünülebilir.
+                bool guarantorIsFine = true;
+
+                base.CreditConfirmed = guarantorIsFine;
+            }
+        }
+    }
+}

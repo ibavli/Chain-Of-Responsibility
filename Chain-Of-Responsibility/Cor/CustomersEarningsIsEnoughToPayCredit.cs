bc8fb96587b4a76e92ad8d753c9cea382a5f5d12
ibavli <ibrahim.bavli35@gmail.com>
2019-04-03 00:08:47 +0300

    Müşterinin aylık geliri istenen krediyi ödemeye yeterli mi kontrolü yapıldı

---
+++ Chain-Of-Responsibility/Cor/CustomersEarningsIsEnoughToPayCredit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_Of_Responsibility.Entities;
+
+namespace Chain_Of_Responsibility.Cor
+{
+    public class CustomersEarningsIsEnoughToPayCredit : CreditRequestBase
+    {
+        public override void ExecuteProcess(Customer customer)
+        {
+            //Bir önceki adımdan geçti mi?
+            if (base.CreditConfirmed)
+            {
+                //Gelen müşteri bilgilerinden yola çıkılarak maaşının kredi taksitlerini ödemeye yeterli olup olmadığı kontrolü yapılır.
+                bool isEnoughToPay = true;
+
+                base.CreditConfirmed = isEnoughToPay;
+            }
+        }
+    }
+}

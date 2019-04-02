033666a1cc382fda133453197507aa7558da27e2
ibavli <ibrahim.bavli35@gmail.com>
2019-04-03 00:04:41 +0300

    Daha önceden kredi kartı var mı, varsa kredi kartına ait borçları ne kadar süreyle geciktirmiş? Bu geciktirme süresi 3 aydan fazla ise direkt ret ver.

---
+++ Chain-Of-Responsibility/Cor/CheckKreditCardPaymentTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_Of_Responsibility.Entities;
+
+namespace Chain_Of_Responsibility.Cor
+{
+    public class CheckKreditCardPaymentTime : CreditRequestBase
+    {
+        public override void ExecuteProcess(Customer customer)
+        {
+            //Bir önceki adımdan geçti mi?
+            if (base.CreditConfirmed)
+            {
+                //Burada daha önce kredi kartı borcunu geciktirip gecirtirmediği ve geciktirdiyse kaç ay geciktirdiği verisi getiriliyor.
+                int paymetDelayTime = 1;
+
+                base.CreditConfirmed = paymetDelayTime > 3 ? false : true;
+            }
+        }
+    }
+}
